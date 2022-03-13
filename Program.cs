afae05b75e5258e75aed2fa612797e39a6127a40
Mateusz Piotr Moruś <mpmorus@gmail.com>
2022-03-13 21:07:22 +0100

    Remove unwanted files from generated template

--- Program.cs
+++ Program.cs
@@ -1,13 +1,18 @@
-using System;
-using System.IO;
 using AdventOfCode.YearYYYY.DayTemplate;
 
 const string DEFAULT_INPUT_FILEPATH = "input.txt";
 
-string filepath = args.Length > 0 ? args[0] : DEFAULT_INPUT_FILEPATH;
-
 try
 {
+	string filepath = args.Length switch
+	{
+		0 => DEFAULT_INPUT_FILEPATH,
+		1 => args[0],
+		_ => throw new ApplicationException(
+			$"Program was called with too many arguments. Proper usage: \"dotnet run [<input filepath>]\"."
+		)
+	};
+
 	var solver = new DayTemplateSolver(filepath);
 
 	Console.Write("Part 1: ");
@@ -18,11 +23,19 @@ try
 	string part2 = solver.SolvePart2();
 	Console.WriteLine(part2);
 }
-catch (FileNotFoundException)
+catch (FileNotFoundException e)
 {
 	ConsoleColor previousColor = Console.ForegroundColor;
 	Console.ForegroundColor = ConsoleColor.Red;
-	Console.Error.WriteLine($"Could not find file `{filepath}`.");
+	Console.Error.WriteLine(e.Message);
+	Console.ForegroundColor = previousColor;
+	Environment.Exit(1);
+}
+catch (ApplicationException e)
+{
+	ConsoleColor previousColor = Console.ForegroundColor;
+	Console.ForegroundColor = ConsoleColor.Red;
+	Console.Error.WriteLine($"Error: {e.Message}");
 	Console.ForegroundColor = previousColor;
 	Environment.Exit(1);
 }
