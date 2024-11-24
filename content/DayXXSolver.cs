3088d49c7b8149f65356d6fa5dd7534d6831a2d3
Mateusz Moruś <mpmorus@gmail.com>
2024-11-24 19:41:19 +0100

    Update to use .NET 9 and new package (#56)
    * Update to use .NET 9 and new package
    
    * Update workflows to use .NET 9

--- content/DayXXSolver.cs
+++ content/DayXXSolver.cs
@@ -1,8 +1,8 @@
-using AdventOfCode.Abstractions;
+using AdventOfCode.Common;
 
 namespace AdventOfCode.YearYYYY.DayXX;
 
-public sealed class DayXXSolver : DaySolver
+public sealed class DayXXSolver : DaySolver<DayXXSolverOptions>
 {
 	public override int Year => YYYY;
 	public override int Day => XX;
@@ -11,12 +11,12 @@ public sealed class DayXXSolver : DaySolver
 	public DayXXSolver(DayXXSolverOptions options) : base(options)
 	{
 		// Initialize DayXX solver here.
+		// Property `Options` contains options passed to this constructor that were forwarded to the base constructor.
 		// Property `Input` contains the raw input text.
 		// Property `InputLines` enumerates lines in the input text.
 	}
 
-	public DayXXSolver(Action<DayXXSolverOptions> configure)
-		: this(DaySolverOptions.FromConfigureAction(configure))
+	public DayXXSolver(Action<DayXXSolverOptions> configure) : this(DaySolverOptions.FromConfigureAction(configure))
 	{
 	}
 
--- content/Program.cs
+++ content/Program.cs
@@ -1,5 +1,5 @@
 using System.Diagnostics;
-using AdventOfCode;
+using AdventOfCode.Common;
 using AdventOfCode.YearYYYY.DayXX;
 
 try
