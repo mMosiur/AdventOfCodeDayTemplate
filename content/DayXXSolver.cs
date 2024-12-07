806e4649141ebdc618d2dbf55d3c5a492c3caec6
Mateusz Moruś <mpmorus@gmail.com>
2024-12-07 14:11:16 +0100

    Changed indentation to spaces (#63)

--- content/DayXXSolver.cs
+++ content/DayXXSolver.cs
@@ -4,33 +4,33 @@ namespace AdventOfCode.YearYYYY.DayXX;
 
 public sealed class DayXXSolver : DaySolver<DayXXSolverOptions>
 {
-	public override int Year => YYYY;
-	public override int Day => XX;
-	public override string Title => "TITLE";
+    public override int Year => YYYY;
+    public override int Day => XX;
+    public override string Title => "TITLE";
 
-	public DayXXSolver(DayXXSolverOptions options) : base(options)
-	{
-		// Initialize DayXX solver here.
-		// Property `Options` contains options passed to this constructor that were forwarded to the base constructor.
-		// Property `Input` contains the raw input text.
-		// Property `InputLines` enumerates lines in the input text.
-	}
+    public DayXXSolver(DayXXSolverOptions options) : base(options)
+    {
+        // Initialize DayXX solver here.
+        // Property `Options` contains options passed to this constructor that were forwarded to the base constructor.
+        // Property `Input` contains the raw input text.
+        // Property `InputLines` enumerates lines in the input text.
+    }
 
-	public DayXXSolver(Action<DayXXSolverOptions> configure) : this(DaySolverOptions.FromConfigureAction(configure))
-	{
-	}
+    public DayXXSolver(Action<DayXXSolverOptions> configure) : this(DaySolverOptions.FromConfigureAction(configure))
+    {
+    }
 
-	public DayXXSolver() : this(new DayXXSolverOptions())
-	{
-	}
+    public DayXXSolver() : this(new DayXXSolverOptions())
+    {
+    }
 
-	public override string SolvePart1()
-	{
-		return "UNSOLVED";
-	}
+    public override string SolvePart1()
+    {
+        return "UNSOLVED";
+    }
 
-	public override string SolvePart2()
-	{
-		return "UNSOLVED";
-	}
+    public override string SolvePart2()
+    {
+        return "UNSOLVED";
+    }
 }
--- content/Program.cs
+++ content/Program.cs
@@ -4,42 +4,42 @@ using AdventOfCode.YearYYYY.DayXX;
 
 try
 {
-	string? filepath = args.Length switch
-	{
-		0 => null,
-		1 => args[0],
-		_ => throw new CommandLineException(
-			$"Program was called with too many arguments. Proper usage: \"dotnet run [<input filepath>]\"."
-		)
-	};
+    string? filepath = args.Length switch
+    {
+        0 => null,
+        1 => args[0],
+        _ => throw new CommandLineException(
+            $"Program was called with too many arguments. Proper usage: \"dotnet run [<input filepath>]\"."
+        )
+    };
 
-	DayXXSolver solver = new(options =>
-	{
-		options.InputFilepath = filepath ?? options.InputFilepath;
-	});
+    DayXXSolver solver = new(options =>
+    {
+        options.InputFilepath = filepath ?? options.InputFilepath;
+    });
 
-	Console.WriteLine($"--- Day {solver.Day}: {solver.Title} ---");
+    Console.WriteLine($"--- Day {solver.Day}: {solver.Title} ---");
 
-	Console.Write("Part one: ");
-	string part1 = solver.SolvePart1();
-	Console.WriteLine(part1);
+    Console.Write("Part one: ");
+    string part1 = solver.SolvePart1();
+    Console.WriteLine(part1);
 
-	Console.Write("Part two: ");
-	string part2 = solver.SolvePart2();
-	Console.WriteLine(part2);
+    Console.Write("Part two: ");
+    string part2 = solver.SolvePart2();
+    Console.WriteLine(part2);
 }
 catch (AdventOfCodeException e)
 {
-	string errorPrefix = e switch
-	{
-		CommandLineException => "Command line error",
-		InputException => "Input error",
-		DaySolverException => "Day solver error",
-		_ => throw new UnreachableException($"Unknown exception type \"{e.GetType()}\".")
-	};
+    string errorPrefix = e switch
+    {
+        CommandLineException => "Command line error",
+        InputException => "Input error",
+        DaySolverException => "Day solver error",
+        _ => throw new UnreachableException($"Unknown exception type \"{e.GetType()}\".")
+    };
 
-	Console.ForegroundColor = ConsoleColor.Red;
-	Console.Error.WriteLine($"{errorPrefix}: {e.Message}");
-	Console.ResetColor();
-	Environment.Exit(1);
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Error.WriteLine($"{errorPrefix}: {e.Message}");
+    Console.ResetColor();
+    Environment.Exit(1);
 }
